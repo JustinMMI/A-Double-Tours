0d63ec4a6cb6d387d1cf365cf1b0a22fe62e08d8
JuJust123 <justin.esquer@mmibordeaux.com>
2026-04-02 10:55:53 +0200

    Title Screen

--- Assets/Scripts/PionSelector.cs
+++ Assets/Scripts/PionSelector.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class GameManagerScript : MonoBehaviour
+public class PionSelector : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
